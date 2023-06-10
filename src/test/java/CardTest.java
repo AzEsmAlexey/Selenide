3584cd09eb83074e3b9f5820ad8c092eee0d7c80
Neto.Changes <infero-sky@yandex.ru>
2023-06-10 22:31:05 +0300

    maybe final

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
@@ -5,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,20 +21,22 @@ import static com.codeborne.selenide.SetValueOptions.withText;
 
 public class CardTest {
 
+
+
     @Test
     void shouldRegisterForm() {
         //Configuration.holdBrowserOpen = true;
+        DataGenerateService planningDate = new DataGenerateService();
 
         open("http://localhost:9999/");
         $("[data-test-id=\"city\"] input").setValue("Москва");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate.generateDate(10));
         $("[data-test-id=\"name\"] input").setValue("Вещий Олег");
-        $("[data-test-id=\"date\"] input").click();
-        $("[data-test-id=\"date\"] input").clear();
-        $("[data-test-id=\"date\"] input").setValue("22122023");
         $("[data-test-id=\"phone\"] input").setValue("+79024417754");
         $("[data-test-id=\"agreement\"]").click();
         $(byText("Забронировать")).click();
-        $("[data-test-id=\"notification\"]").shouldBe(visible, Duration.ofSeconds(12));
+        $("[data-test-id=\"notification\"]").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate.generateDate(10)), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
 }
